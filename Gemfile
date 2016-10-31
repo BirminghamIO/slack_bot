ecf9ab6eb25d90aecb5fe8fdfb4a5aee4fa21126
Daniel Hollands <daniel@limeblast.co.uk>
2016-10-31 21:55:38 +0000

    Basic (and crap) implementation of minimum requirements, I hope

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 ruby '2.3.1'
 gem 'sinatra', '~> 1.4.7'
+gem 'sinatra-contrib', '~> 1.4.7'
 gem 'thin', '~> 1.7.0'
 gem 'appsignal', '~> 2.0.0'
--- web.rb
+++ web.rb
@@ -1,10 +1,52 @@
 require 'sinatra'
 require 'appsignal/integrations/sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/json'
 
 get '/' do
-  "Hello, world"
+  redirect 'https://birmingham.io/', 301
 end
 
 post '/' do
-  "How's this?"
+  json send request_action
+end
+
+private
+
+# request processing stuff
+def request_payload
+  @request_payload ||= begin
+    request.body.rewind
+    JSON.parse request.body.read
+  end
+end
+
+def request_action
+  @request_action ||= request_payload['result']['action']
+end
+
+def request_parameters
+  @request_parameters ||= request_payload['result']['parameters']
+end
+
+# getting data stuff
+def forum_search
+  format_return "Forum search for '#{request_parameters['search_term']}'"
+end
+
+def forum_latest
+  format_return 'Latest topics'
+end
+
+def forum_top
+  format_return 'Top topics'
+end
+
+# returning data stuff
+def format_return(string)
+  {
+      'speech':      string,
+      'displayText': string,
+      'source':      "birmingham.io"
+  }
 end
